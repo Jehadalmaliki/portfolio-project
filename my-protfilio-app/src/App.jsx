fbbec45f98e7bfb59166e22eb9eb6427f04f72da
Jehad Almaliki <yourgitemail@example.com>
2023-03-05 23:22:43 +0300

    feat : SplitScreen to tow

--- my-protfilio-app/src/App.jsx
+++ my-protfilio-app/src/App.jsx
@@ -1,34 +1,110 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import * as React from "react";
+import { motion } from "framer-motion";
 
+
+export default function App() {
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div >
+      <SplitScreenOverride />
     </div>
-  )
+  );
 }
 
-export default App
+function SplitScreen(props) {
+  return (
+    <motion.div
+      style={{
+        display: "flex",
+        backgroundColor: "#ccc",
+        height: "calc(100vh - 60px)",
+        flexDirection: props.direction,
+      }}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+    >
+      <motion.div
+        style={{
+          flex: 1,
+          backgroundColor: "#fdd",
+          height: "100%",
+  
+    
+        }}
+      >
+        {props.left}
+      </motion.div>
+      <motion.div
+        style={{
+          flex: 1,
+          height: "100%",
+  
+        }}
+      >
+        {props.right}
+      </motion.div>
+    </motion.div>
+  );
+}
+
+export function SplitScreenOverride()  {
+  const [isSmallScreen, setIsSmallScreen] = React.useState(false);
+
+  React.useEffect(() => {
+    function handleResize() {
+      setIsSmallScreen(window.innerWidth < 768);
+    }
+
+    window.addEventListener("resize", handleResize);
+    handleResize();
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return (
+    <ErrorBoundary>
+      {isSmallScreen ? (
+        <SplitScreen
+          direction="column"
+          left={<div style={{ marginBottom: "20px" }}></div>}
+          right={<div>This is the right column.</div>}
+        />
+      ) : (
+        <SplitScreen
+          direction="row"
+          left={<div style={{ marginRight: "20px" }}></div>}
+          right={<div>This is the right column.</div>}
+        />
+      )}
+    </ErrorBoundary>
+  );
+}
+
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    // You can also log the error to an error reporting service
+    console.error(error, errorInfo);
+  }
+  render() {
+    if (this.state.hasError) {
+      // Fallback UI for error state
+      return <div>Something went wrong.</div>;
+    }
+
+    // Render children
+
+    return this.props.children;
+  }
+}
