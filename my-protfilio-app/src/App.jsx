02732ccd9886ae6b45925a3f1890cf1e9e14dc64
Jehad Almaliki <yourgitemail@example.com>
2023-03-06 23:28:44 +0300

    feat : add router to the pages

--- my-protfilio-app/src/App.jsx
+++ my-protfilio-app/src/App.jsx
@@ -1,12 +1,30 @@
 import * as React from "react";
 
-import { SplitScreenOverride } from "./componate/splitScreen/SplitScreenOverride";
+import { Navbar } from "./componate/navbar/Navbar";
+import { BrowserRouter,Route,Routes  } from "react-router-dom";
+import { Home } from "./componate/pages/Home";
+import { About } from "./componate/pages/About";
+import {Works} from "./componate/pages/Works";
+import {Contact} from "./componate/pages/Contact";
+
 export default function App() {
-  return (
-    <div>
-      <SplitScreenOverride />
-    </div>
-  );
-}
+ 
+      return (
+       <BrowserRouter> 
+           <Navbar />
+        <Routes> 
+          <Route exact path="/" element={<Home/>} />
+          <Route path="/about" element={<About/>} />
+          <Route path="/contact" element={<Contact/>} />
+          <Route path="/works" element={<Works/>} />
+
+          </Routes>
+      
+        </BrowserRouter>
+      );
+    }
+    
+
+
 
 
