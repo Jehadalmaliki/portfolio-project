ffce19568eec3766ff7a26632dd2e4bdd0f65c81
Jehad Almaliki <yourgitemail@example.com>
2023-03-07 14:03:26 +0300

    new : create left nav pages

---
+++ my-protfilio-app/src/componate/lft/navbarLft/NavbarLft.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import { motion } from "framer-motion";
+import { Link } from "react-router-dom";
+import { NavbarLtlstyles } from "./NavbarLtl.style";
+export function NavbarLft() {
+  const [hovered, setHovered] = useState(null);
+
+  const handleMouseEnter = (index) => {
+    setHovered(index);
+  };
+
+  const handleMouseLeave = () => {
+    setHovered(null);
+  };
+  return (
+    <motion.nav style={NavbarLtlstyles.nav}>
+        <motion.ul style={NavbarLtlstyles.ul }>
+        <motion.li
+          style={
+            hovered === 0 || window.location.pathname === "/"
+              ? { ...NavbarLtlstyles.li, ...NavbarLtlstyles.liHover }
+              : NavbarLtlstyles.li
+          }
+          onMouseEnter={() => handleMouseEnter(0)}
+          onMouseLeave={() => handleMouseLeave()}
+        >
+          <Link
+            to="/"
+            style={hovered === 0 ? NavbarLtlstyles.aHover : NavbarLtlstyles.a}
+          >
+            J | A
+            <span
+              style={
+                hovered === 0 ? NavbarLtlstyles.aHoverBefore : NavbarLtlstyles.aBefore
+              }
+            ></span>
+          </Link>
+        </motion.li>
+        </motion.ul>
+      
+      <motion.ul style={NavbarLtlstyles.ul}>      
+      </motion.ul>
+      
+    </motion.nav>
+  );
+}
