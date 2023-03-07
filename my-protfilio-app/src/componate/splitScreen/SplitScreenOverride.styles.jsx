9be4e8706024daad4dd528626200ae8204fe4782
Jehad Almaliki <yourgitemail@example.com>
2023-03-07 14:02:45 +0300

    feat : add media query so make it responcive

--- my-protfilio-app/src/componate/splitScreen/SplitScreenOverride.styles.jsx
+++ my-protfilio-app/src/componate/splitScreen/SplitScreenOverride.styles.jsx
@@ -1,19 +1,28 @@
 export const styles = {
-    container: {
-      display: "flex",
-      backgroundColor: "#ccc",
-      height: "calc(100vh - 60px)",
+  container: {
+    display: "flex",
+    backgroundColor: "#031c07",
+    height: "calc(100vh - 20px)",
+    padding: 0,
+    margin: 0,
+  },
+  leftPane: {
+    flex: 1,
+    backgroundColor: "#004d1f",
+    height: "100%",
+    opacity: 0,
+    padding: 0,
+    margin: 0,
+    borderRight: "1px solid #43ee61",
+    "@media(max-width: 767px)": {
+      borderRight: "none",
     },
-    leftPane: {
-      flex: 1,
-      backgroundColor: "#fdd",
-      height: "100%",
-      opacity: 0,
-    },
-    rightPane: {
-      flex: 1,
-      height: "100%",
-      opacity: 0,
-    },
-  };
-  
+  },
+  rightPane: {
+    flex: 1,
+    height: "100%",
+    opacity: 0,
+    padding: 0,
+    margin: 0,
+  },
+};
