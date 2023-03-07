16fc2607ae6a0c390b17772399694605c3d42686
Jehad Almaliki <yourgitemail@example.com>
2023-03-07 14:01:56 +0300

    feat : edit margin on small screen

--- my-protfilio-app/src/componate/splitScreen/SplitScreenOverride.jsx
+++ my-protfilio-app/src/componate/splitScreen/SplitScreenOverride.jsx
@@ -1,72 +1,70 @@
-
 import * as React from "react";
 import { motion } from "framer-motion";
 import { styles } from "./SplitScreenOverride.styles";
 import SplitScreen from "./SplitScreen";
 import { ErrorBoundary } from "./ErrorBoundary";
+import { Navbar } from "../rtl/navbarRlt/NavbarRtl";
+import { NavbarLft } from "../lft/navbarLft/NavbarLft";
 
 export function SplitScreenOverride() {
-    const [isSmallScreen, setIsSmallScreen] = React.useState(false);
-  
-    React.useEffect(() => {
-      function handleResize() {
-        setIsSmallScreen(window.innerWidth < 768);
-      }
-  
-      window.addEventListener("resize", handleResize);
-      handleResize();
-  
-      return () => {
-        window.removeEventListener("resize", handleResize);
-      };
-    }, []);
-  
-    return (
-      <ErrorBoundary>
-        {isSmallScreen ? (
-          <SplitScreen
-            direction="column"
-            left={
-              <motion.div
-                style={{ marginBottom: "20px", ...styles.leftPane}}
-                animate={{ opacity: 1, y: 0 }}
-                transition={{ duration: 0.5, delay: 0.5 }}
-              >
-                Left 
-              </motion.div>
-            }
-            right={
-              <motion.div
-                style={{ opacity: 0 ,  ...styles.rightPane,}}
-                animate={{ opacity: 1 }}
-                transition={{ duration: 0.5, delay: 0.5 }}
-              >
-                Right 
-              </motion.div>
-            }
-          />
-        ) : (
-          <SplitScreen
-            direction="row"
-            left={
-              <motion.div
-                style={{ marginRight: "20px", ...styles.leftPane, x: -20 }}
-                animate={{ opacity: 1, x: 0 }}
-                transition={{ duration: 0.3, delay: 0.5 }}
-              >
-             Column
-              </motion.div>
-            }
-  
-            right={<motion.div
-              style={{ marginRight: "20px", opacity: 0, x: -20 }}
-              animate={{ opacity: 1, x: 0 }}
-              transition={{ duration: 0.1, delay: 0.5 }}
+  const [isSmallScreen, setIsSmallScreen] = React.useState(false);
+
+  React.useEffect(() => {
+    function handleResize() {
+      setIsSmallScreen(window.innerWidth < 768);
+    }
+
+    window.addEventListener("resize", handleResize);
+    handleResize();
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return (
+    <ErrorBoundary>
+      {isSmallScreen ? (
+        <SplitScreen
+          direction="column"
+          left={
+            <motion.div
+              style={{  ...styles.leftPane }}
+              animate={{ opacity: 1, y: 0 }}
             >
-       Column
-            </motion.div>}
-          />
-        )}
-      </ErrorBoundary>
-    );
-  }
+              <NavbarLft />
+            </motion.div>
+          }
+          right={
+            <motion.div
+              style={{ opacity: 0, ...styles.rightPane }}
+              animate={{ opacity: 1 }}
+            >
+              <Navbar />
+            </motion.div>
+          }
+        />
+      ) : (
+        <SplitScreen
+          direction="row"
+          left={
+            <motion.div
+              style={{  ...styles.leftPane }}
+              animate={{ opacity: 1, x: 0 }}
+            >
+              <NavbarLft />
+            </motion.div>
+          }
+          right={
+            <motion.div
+              style={{ marginRight: "20px", opacity: 0 }}
+              animate={{ opacity: 1, x: 0 }}
+            >
+              <Navbar />
+            </motion.div>
+          }
+        />
+      )}
+    </ErrorBoundary>
+  );
+}
