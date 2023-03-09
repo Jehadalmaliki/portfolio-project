66849083e105a50376a4d81870873cbfe54730c1
Jehad Almaliki <yourgitemail@example.com>
2023-03-09 12:50:22 +0300

    feat : rename file from small to capital letter

---
+++ my-protfilio-app/src/componate/RTL/NavbarRTL/NavbarRtl.styles.jsx
@@ -0,0 +1,57 @@
+export const Navbarstyles = {
+    nav: {
+      display: "flex",    
+      justifyContent: "space-between",
+      height: "8vh",
+ 
+    },
+    ul: {
+      display: "flex",
+      listStyle: "none", 
+          
+    },
+    li: {
+      margin: "0rem 3rem",
+      position: "relative",
+      transition: "border-color 0.2s ease-in-out",   
+    },
+    liHover: {
+      borderBottom: "1px solid transparent",
+      borderBottomColor: "#43ee61",
+      transition: "borderBottomColor 0.9s ease-in-out",
+      textTransform: "uppercase",      
+    },
+    a: {
+      display: "block",
+      position: "relative",
+      textDecoration: "none",
+      padding: "5px",
+      fontSize: "18px",
+      fontFamily: "sans-serif",
+      color: "#43ee61",
+      textTransform: "uppercase",
+     
+    },
+    aBefore: {
+      position: "absolute",
+      top: "100%",
+      left: "0",
+      width: "100%",
+      height: "2px",
+      backgroundColor: "#43ee61",
+      content: '""',
+      opacity: "0",
+      
+    },
+    aHover: {
+      color: "#43ee61",
+      zIndex: "1",
+      textDecoration: "none" 
+      
+    },
+    aHoverBefore: {
+      opacity: "1",
+    },
+   
+  };
+  
