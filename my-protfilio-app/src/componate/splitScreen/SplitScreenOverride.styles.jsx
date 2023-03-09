e0ea2d25ba9d46617c00aaed962adee56f06ebe1
Jehad Almaliki <yourgitemail@example.com>
2023-03-09 12:48:00 +0300

    style : modife hight to fit the parallax

--- my-protfilio-app/src/componate/splitScreen/SplitScreenOverride.styles.jsx
+++ my-protfilio-app/src/componate/splitScreen/SplitScreenOverride.styles.jsx
@@ -1,28 +1,29 @@
 export const styles = {
   container: {
     display: "flex",
-    backgroundColor: "#031c07",
-    height: "calc(100vh - 20px)",
-    padding: 0,
-    margin: 0,
+    backgroundColor: "#43ee61",
+    height: "calc(100vh - 0px)",
+  
   },
   leftPane: {
-    flex: 1,
-    backgroundColor: "#004d1f",
+    flex: 1,  
     height: "100%",
     opacity: 0,
     padding: 0,
     margin: 0,
-    borderRight: "1px solid #43ee61",
-    "@media(max-width: 767px)": {
-      borderRight: "none",
-    },
+
+  // borderRight: "1px solid #43ee61",
+  //   "@media(max-width: 767px)": {
+  //     borderRight: "none",
+  //   },
   },
   rightPane: {
+ 
     flex: 1,
     height: "100%",
     opacity: 0,
     padding: 0,
     margin: 0,
+    backgroundColor: "#031c07",
   },
 };
