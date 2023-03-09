74c941c8c7e8a596756b50cde4c89515a92bea24
Jehad Almaliki <yourgitemail@example.com>
2023-03-09 12:47:11 +0300

    feat : add parallax to left side larag screen

--- my-protfilio-app/src/componate/splitScreen/SplitScreenOverride.jsx
+++ my-protfilio-app/src/componate/splitScreen/SplitScreenOverride.jsx
@@ -1,10 +1,14 @@
 import * as React from "react";
-import { motion } from "framer-motion";
+import { motion, useTransform } from "framer-motion";
 import { styles } from "./SplitScreenOverride.styles";
 import SplitScreen from "./SplitScreen";
 import { ErrorBoundary } from "./ErrorBoundary";
-import { Navbar } from "../rtl/navbarRlt/NavbarRtl";
-import { NavbarLft } from "../lft/navbarLft/NavbarLft";
+import { Navbar } from "../RTL/NavbarRTL/NavbarRtl";
+import { NavbarLft } from "../LTR/NavbarLTR/NavbarLtr";
+import { useState } from "react";
+// import ParallaxBackground from "./ParallaxBackground";
+// import Parall from "./Parall";
+import Reactspring from "../LTR/ParallaxLTR/Reactspring";
 
 export function SplitScreenOverride() {
   const [isSmallScreen, setIsSmallScreen] = React.useState(false);
@@ -29,10 +33,13 @@ export function SplitScreenOverride() {
           direction="column"
           left={
             <motion.div
-              style={{  ...styles.leftPane }}
-              animate={{ opacity: 1, y: 0 }}
-            >
-              <NavbarLft />
+              style={{ ...styles.leftPane }}
+              initial={{ opacity: 0.7}}
+            
+              animate={{ opacity: 1, x: 0 }}
+              
+            > 
+                <NavbarLft />       
             </motion.div>
           }
           right={
@@ -49,18 +56,30 @@ export function SplitScreenOverride() {
           direction="row"
           left={
             <motion.div
-              style={{  ...styles.leftPane }}
+              style={{ ...styles.leftPane }}    
+              initial={{  x: 20 }}  
               animate={{ opacity: 1, x: 0 }}
+              transition={{ duration: 0.5 }}
             >
-              <NavbarLft />
+         
+          <Reactspring/>
+                <NavbarLft />
             </motion.div>
           }
           right={
             <motion.div
-              style={{ marginRight: "20px", opacity: 0 }}
+              style={{ marginRight: "10px", opacity: 0 }}
+              initial={{  x: -12 }}  
               animate={{ opacity: 1, x: 0 }}
+              transition={{ duration: 0.5 }}
             >
+           
               <Navbar />
+              <div style={{ color:"#43ee61" ,display: 'flex', justifyContent: 'center', alignItems: 'center', height: '50vh' }}>
+      <div className={styles.title}>
+        <span style={{ fontSize: 60, textTransform: 'capitalize' }}>Jehad Ali</span>
+      </div>
+      </div>
             </motion.div>
           }
         />
