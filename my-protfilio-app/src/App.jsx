9336139be51da562224e83e9c1691d265ac7cf3e
Jehad Almaliki <yourgitemail@example.com>
2023-03-09 12:40:06 +0300

    Formate file

--- my-protfilio-app/src/App.jsx
+++ my-protfilio-app/src/App.jsx
@@ -1,25 +1,18 @@
 import * as React from "react";
-
-
 import { BrowserRouter,Route,Routes  } from "react-router-dom";
-import { Home } from "./componate/pages/Home";
-import { About } from "./componate/pages/About";
-import {Works} from "./componate/pages/Works";
-import {Contact} from "./componate/pages/Contact";
-
+import { Home } from "./componate/Pages/Home";
+import { About } from "./componate/Pages/About";
+import {Works} from "./componate/Pages/Works";
+import {Contact} from "./componate/Pages/Contact";
 export default function App() {
- 
       return (
-       <BrowserRouter> 
-      
+       <BrowserRouter>      
         <Routes> 
           <Route exact path="/" element={<Home/>} />
           <Route path="/about" element={<About/>} />
           <Route path="/contact" element={<Contact/>} />
           <Route path="/works" element={<Works/>} />
-
-          </Routes>
-      
+          </Routes>    
         </BrowserRouter>
       );
     }
