fe36ef7a59b8a81d25d42249062a19a49e3cf41f
Jehad Almaliki <yourgitemail@example.com>
2023-03-31 10:23:54 +0300

    feat: add more pages

--- src/componate/Pages/Home/ParallaxLTR/index.jsx
+++ src/componate/Pages/Home/ParallaxLTR/index.jsx
@@ -1,6 +1,7 @@
 import React, { useRef } from "react";
+import { Parallax, ParallaxLayer } from "@react-spring/parallax";
+import { animated } from "@react-spring/web";
 
-import { motion, useScroll } from "framer-motion"
 import styles from "./styles.module.css";
 import img1 from "../../../../assets/images/Babor.png";
 import img2 from "../../../../assets/images/madLab.png";
@@ -10,53 +11,109 @@ import img5 from "../../../../assets/images/job.png";
 import img6 from "../../../../assets/images/des.png"
 
 
-const Reactspring = () => {
+const Page = ({ offset }) => (
+  <>
+    <ParallaxLayer offset={offset} speed={0.6}>
+      <div className={styles.slopeBegin} />
+    </ParallaxLayer>
+  </>
+);
 
-  const { scrollYProgress } = useScroll();
-  
+const Reactspring = () => {
+  const parallax = useRef(null);
+
+  const scroll = (to) => {
+    if (parallax.current) {
+      parallax.current.scrollTo(to);
+    }
+  };
 
   return (
-    <motion.div className={styles.container}   >
-      <div className={styles.page}>
-        <div
-          className={styles.parallaxLayer}
-          style={{
-            backgroundImage: `url(${img4})`,
-            filter: "grayscale(60%)",
-          }}
+    <div >
+      <div style={{ background: "#dfdfdf" }}>
+        <Parallax
+          className={styles.container}
+          ref={parallax}
+          pages={4}
+          vertical
         >
-          <div className={styles.textOverlay}>
+          <Page offset={0} />
+          <ParallaxLayer
+            offset={0}
+            speed={0.5}
+            style={{
+              backgroundImage: `url(${img4})`,
+              backgroundSize: "contain",
+              width: "49.5vw",
+              height:"auto",
+              filter: "grayscale(50%)",
+             
+            }}
+            
+          />
+          <ParallaxLayer className={styles.textOverlay} offset={0.2} speed={1}>
             <span className={styles.text}>Movie Project</span>
-          </div>
-        </div>
+          </ParallaxLayer>
+
+          <Page offset={1} />
+          <ParallaxLayer
+            offset={1}
+            speed={1}
+            style={{
+              backgroundImage: `url(${img6})`,
+              backgroundSize: "contain",
+              width: "49.5vw",
+              height:"auto",
+              filter: "grayscale(50%)",
+            }}
+            
+          />
+          <ParallaxLayer className={styles.textOverlay} offset={1} speed={3}>
+            <span className={styles.text}>Babor  Project </span>
+          </ParallaxLayer>
+
+          <Page offset={2} />
+          <ParallaxLayer
+            offset={2}
+            speed={0.1}
+            style={{
+              backgroundImage: `url(${img2})`,
+              backgroundSize: "contain",
+              width: "49.5vw",
+              height:"auto",
+              filter: "grayscale(50%)",
+            
+                
+            }}
+            className={styles.img5}
+          />
+
+          <ParallaxLayer className={styles.textOverlay} offset={2} speed={2}>
+            <span className={styles.text}>Wazeef pROJECT</span>
+          </ParallaxLayer>
+
+          <Page offset={3} />
+          <ParallaxLayer
+            offset={3}
+            speed={0.5}
+            style={{
+              backgroundImage: `url(${img6})`,
+              backgroundSize: "contain",
+              width: "49.5vw",
+              height:"auto",
+              filter: "grayscale(50%)",
+            
+                
+            }}
+            className={styles.img5}
+          />
+
+          <ParallaxLayer className={styles.textOverlay} offset={3} speed={1}>
+            <span className={styles.text}>Wazeef pROJECT</span>
+          </ParallaxLayer>
+        </Parallax>
       </div>
-      <div className={styles.page}>
-        <div
-          className={styles.parallaxLayer}
-          style={{
-            backgroundImage: `url(${img6})`,
-            filter: "grayscale(60%)",
-          }}
-        >
-          <div className={styles.textOverlay}>
-            <span className={styles.text}>Babor Auction Project</span>
-          </div>
-        </div>
-      </div>
-      <div className={styles.page}>
-        <div
-          className={styles.parallaxLayer}
-          style={{
-            backgroundImage: `url(${img2})`,
-            filter: "grayscale(30%)",
-          }}
-        >
-          <div className={styles.textOverlay}>
-            <span className={styles.text}>Wazeef Job Project</span>
-          </div>
-        </div>
-      </div>
-    </motion.div>
+    </div>
   );
 };
 
