45a0521388a71bc13cd4aee475f3598a284907c9
Jehad Almaliki <yourgitemail@example.com>
2023-05-23 11:35:57 +0300

    feat: add contact us page for desktop

---
+++ src/componate/Pages/Home/ContactMe/index.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import cv from "../../../../assets/contact-icons/cv.svg"
+import email from "../../../../assets/contact-icons/email.svg"
+import github from "../../../../assets/contact-icons/github.svg"
+import linkedin from "../../../../assets/contact-icons/linkedin.svg"
+import twitter from "../../../../assets/contact-icons/twitter.svg"
+import './style.css';
+export default function Contact
+() {
+  return (
+    <div className="icon-container">
+       <a href="https://app.enhancv.com/share/35d533d2/?utm_medium=growth&utm_campaign=share-resume&utm_source=dynamic" target="_blank" rel="noopener noreferrer">
+        <img src={cv} alt="CV" className="icon-image" />
+      </a>
+      <a href="mailto:engjehadali5@gmail.com" target="_blank" rel="noopener noreferrer">
+        <img src={email} alt="Email" className="icon-image" />
+      </a>
+      <a href="https://github.com/Jehadalmaliki" target="_blank" rel="noopener noreferrer">
+        <img src={github} alt="GitHub" className="icon-image"/>
+      </a>
+      <a href="https://www.linkedin.com/in/jehad-almaliki-832318183/" target="_blank" rel="noopener noreferrer">
+        <img src={linkedin} alt="LinkedIn" className="icon-image"/>
+      </a>
+      <a href="link_to_twitter" target="_blank" rel="noopener noreferrer">
+        <img src={twitter} alt="Twitter" className="icon-image"/>
+      </a>  
+
+    </div>
+  )
+}
